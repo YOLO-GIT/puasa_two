a472b90484adb542beb43370d0d90b4dabd28d54
YOLO-GIT <ahmadsufi345@gmail.com>
2025-03-20 01:44:10 +0800

    New Assets

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-const contents = gsap.utils.toArray("#horizontal .content");
+// const contents = gsap.utils.toArray("#horizontal .content");
 
-gsap.to(contents, {
-  xPercent: -100 * (contents.length - 1),
-  scrollTrigger: {
-    trigger: "#horizontal",
-    pin: true,
-    scrub: 1,
-  },
-});
+// gsap.to(contents, {
+//   xPercent: -100 * (contents.length - 1),
+//   scrollTrigger: {
+//     trigger: "#horizontal",
+//     pin: true,
+//     scrub: 1,
+//   },
+// });
 
-document.addEventListener("scroll", function () {
-  let scrollPosition = window.scrollY;
-  let parallaxImg = document.querySelector(".parallax");
+// document.addEventListener("scroll", function () {
+//   let scrollPosition = window.scrollY;
+//   let parallaxImg = document.querySelector(".parallax");
 
-  if (parallaxImg) {
-    parallaxImg.style.transform = `translateY(${scrollPosition * 0.3}px)`;
-    // Adjust 0.3 for faster/slower effect
-  }
-});
+//   if (parallaxImg) {
+//     parallaxImg.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+//     // Adjust 0.3 for faster/slower effect
+//   }
+// });
